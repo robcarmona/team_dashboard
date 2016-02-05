cab7cef9a67e777ec2cd153250f0c258355962ef
Robert Carmona <robert.carmona@enspiresoftware.com>
2016-02-05 14:17:41 -0500

    lead seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,8 +3,8 @@ Team.create!([
   ])
 
 User.create!([
-  {name: "Alex Myers", email: "amyers@enspiresoftware.com", scrum_lead_date: Date.today, team_id: 1},
-  {name: "Eric Fox", email: "efox@enspiresoftware.com", scrum_lead_date: Date.today - 1.day, team_id: 1},
-  {name: "Mike Richards", email: "mrichards@enspiresoftware.com", scrum_lead_date: Date.today - 2.day, team_id: 1},
-  {name: "Robert Carmona", email: "rcarmona@enspiresoftware.com", scrum_lead_date: Date.today - 3.day, team_id: 1}
+  {name: "Alex Myers", email: "amyers@enspiresoftware.com", team_id: 1},
+  {name: "Eric Fox", email: "efox@enspiresoftware.com", team_id: 1},
+  {name: "Mike Richards", email: "mrichards@enspiresoftware.com", team_id: 1},
+  {name: "Robert Carmona", email: "rcarmona@enspiresoftware.com", team_id: 1}
 ])
