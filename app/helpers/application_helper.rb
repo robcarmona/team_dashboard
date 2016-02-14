c10ec33b30a1a88a270d937a849c322798dc3f42
Robert Carmona <robert.carmona@enspiresoftware.com>
2016-02-13 22:21:09 -0500

    Updated ui with peer rotation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def gravatar(email, size = 50)
+    url = "http://www.gravatar.com/avatar/"
+    hash = Digest::MD5.hexdigest(email)
+    return "#{url}#{hash}?size=#{size}"
+  end
 end
